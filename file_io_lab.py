4b2aea15f651d701a0f249d3a29957c0f61ef1fb
MUGAIRWA <agalatevinmugairwa@gmail.com>
2025-08-25 19:24:25 +0300

    error handling on opening files

--- file_io_lab.py
+++ file_io_lab.py
@@ -12,3 +12,15 @@ new_file.close()
 
 print("Modified content written to 'output.pdf'")
 
+# File Read & Write with Error Handling 
+# user inputs file name
+filename = input("Enter filename to read: ")
+
+# error handling
+try:
+    file = open(filename, "r")   # try to open the file
+    content = file.read()
+
+    print(" File read successfully")
+except FileNotFoundError:
+    print("Error: The file you entered does not exist.")

e743ba81d593ed36d13e6cdfacfd44adf944d181
MUGAIRWA <agalatevinmugairwa@gmail.com>
2025-08-25 19:17:38 +0300

    new file created with modified content

--- file_io_lab.py
+++ file_io_lab.py
@@ -2,5 +2,13 @@
 file = open("input.pdf", "r")
 content = file.read()
 
+# Modify the content i.e convert to uppercase
+modified_content = content.upper()
 
+# Write the modified content to a new file
+new_file = open("output.pdf", "w")
+new_file.write(modified_content)
+new_file.close()
+
+print("Modified content written to 'output.pdf'")
 
